119cacdb41d0f1cc327f69f35d0fbed74551e633
Sheikh_Ahmed <ssahme21@gmail.com>
2022-12-05 11:39:02 -0500

    Completed Sidebar

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import "./Layout.scss";
+
+import React from "react";
+import Sidebar from "../Sidebar/Sidebar";
+
+const Layout = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,62 @@
+import "./Sidebar.scss";
+import { Link, NavLink } from "react-router-dom";
+import LogoS from "../../assets/images/logo-s.png";
+import LogoSubtitle from "../../assets/images/logo_sub2.png";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="logo-sub" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeClassName="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#696969" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeClassName="active"
+          to="/about"
+          className="about-link"
+        >
+          <FontAwesomeIcon icon={faUser} color="#696969" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeClassName="active"
+          to="/contact"
+          className="contact-link"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#696969" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/sheikhahmed21/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#696969" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/ArcRiseGen132"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#696969" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
